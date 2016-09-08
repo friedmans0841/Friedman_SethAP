ca2a86a58a93544e35b123ec0f3071a304e616a2
friedmans0841 <seth.friedman78@gmail.com>
2016-09-08 09:11:33 -0700

    lesson 1 & 2

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,24 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int q = 5;
+		int w = 8;
+		int mult = q * w;
+		String first = "Seth Friedman";
+		String second = "1234";
+		String third = "Del Mar";
+		String fourth = "92014";
+		int L = 4;
+		int W = 7;
+		int H = 8;
+		int v = 2;
+		int recSurfaceaArea = v * (L * W + L * H + W * H);
+		
+		
+		
+		System.out.println("5 multiplied by 8 is" + " " + mult);
+		System.out.println(first + " " + second + " " + third + " " + fourth);
+		System.out.println("the surface area of your rectangular cube is with legnth being 4 width being 7 and height being 8 is" + " " + recSurfaceaArea);
+	}
+}
