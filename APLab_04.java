9ffb9e65ccdb7cffaadfc79aa4bf67c530b8ff1a
friedmans0841 <seth.friedman78@gmail.com>
2016-09-20 09:16:10 -0700

    edit

--- APLab_04.java
+++ APLab_04.java
@@ -7,7 +7,6 @@ public class APLab_04
 		APLab_04 form = new APLab_04();
 		
 		Scanner keyboard = new Scanner(System.in);
-		System.out.println("<<<<<<<<<<<<<<<<<_Receipt_>>>>>>>>>>>>>>>>>>>\t");
 		
 		System.out.println("Please enter item 1:");
 		String word1 = keyboard.nextLine();
@@ -16,28 +15,38 @@ public class APLab_04
 		
 		
 		System.out.println("Please enter item 2:");
-		String word2 = keyboard.nextLine();
 		String blank1 = keyboard.nextLine();
+		String word2 = keyboard.nextLine();
 		System.out.println("Please enter price 2:");
 		double num2 = keyboard.nextDouble();
 		
 		
 		System.out.println("Please enter item 3:");
-		String word3 = keyboard.nextLine();
 		String blank2 = keyboard.nextLine();
+		String word3 = keyboard.nextLine();
 		System.out.println("Please enter price 3:");
 		double num3 = keyboard.nextDouble();
 		
-		System.out.println("Please enter item 4:");
-		String word4 = keyboard.nextLine();
-		String blank3 = keyboard.nextLine();
-		System.out.println("Please enter price 4:");
-		double num4 = keyboard.nextDouble();
-		
 		System.out.println("<<<<<<<<<<<<<<<<<_Receipt_>>>>>>>>>>>>>>>>>>>\t");
-		form.format(word1, num1);
-		form.format(word2, num2);
-		form.format(word3, num3);
+		
+		form.format("* " + word1, num1);
+		form.format("* " + word2, num2);
+		form.format("* " + word3, num3);
+		System.out.println(" ");
+		System.out.println(" ");
+		double sub = (num1 + num2 + num3);
+		form.format("* " + " Subtotal: " + " ........ ", sub);
+		double tax = (sub * .075);
+		form.format("* " + " Tax: " + " ........ ", tax);
+		double  total = (sub + tax);
+		form.format("* " + " Total: " + " ........ ", total);
+        System.out.println(" ");
+		System.out.println("_________________________________________________");
+		System.out.println("* Thank you for your support *");
+		
+		
+		
+		
 		
 
 		
@@ -46,6 +55,6 @@ public class APLab_04
 	
 	public void format(String word, double num)
 	{
-		System.out.printf("\n%5s %5.2f", word, num);
+		System.out.printf("\n%15s %15.2f", word, num);
 	}
 }
