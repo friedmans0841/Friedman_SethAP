ab67787d73fa79de3d3d9ece43347da62e9e5c68
friedmans0841 <seth.friedman78@gmail.com>
2017-04-27 21:54:05 -0700

    lab 14

---
+++ APLesson_14/APLesson_14/toyota.java
@@ -0,0 +1,29 @@
+public class GMC implements Location
+{
+	private int coorx, coory;
+	
+	public GMC()
+	{
+		coorx = 0;
+		coory = 0;		
+	}
+	
+	public GMC(int x, int y)
+	{
+		coorx = x;
+		coory = y;
+	}
+	
+	public double[] getLoc()
+	{
+		double location[] = new double[2];
+		location[0] = coorx;
+		location[1] = coory;
+		return loction;
+	}	
+	
+	public int getID()
+	{
+		return (int)(Math.random() * 999999) + 1;
+	}
+}
